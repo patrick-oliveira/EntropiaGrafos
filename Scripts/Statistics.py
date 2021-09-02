2c75e438ea8cec4381a39b967f0544f82d0a3274
RedRoboto <patrick.oliveira@aluno.ufabc.edu.br>
2021-09-01 23:48:43 -0300

    Estatisticas programadas, falta testar

--- Scripts/Statistics.py
+++ Scripts/Statistics.py
@@ -1,27 +1,104 @@
-from Scripts.Model import Model
-from typing import Dict, Callable
-from Scripts.Memory import binary_to_string
 import pickle
+import numpy as np
+from Scripts.Model import Model
+from Scripts.Memory import powers_of_two
+from Scripts.Parameters import N, memory_size, code_length
 
 class Statistic:
-    def __call__(self, model: Model) -> float:
+    '''
+    
+    '''
+    def __call__(self, model: Model):
+        '''
+        
+
+        Parameters
+        ----------
+        model : Model
+            DESCRIPTION.
+
+        Raises
+        ------
+        NotImplementedError
+            DESCRIPTION.
+
+        Returns
+        -------
+        None.
+
+        '''
         raise NotImplementedError
         
 class StatisticHandler:
+    '''
+    
+    '''
     def __init__(self):
+        '''
+        
+
+        Returns
+        -------
+        None.
+
+        '''
         self.stats_definitions = {}
         self.stats_values      = {}
 
     def new_statistic(self, name: str, function: Statistic) -> None:
+        '''
+        
+
+        Parameters
+        ----------
+        name : str
+            DESCRIPTION.
+        function : Statistic
+            DESCRIPTION.
+
+        Returns
+        -------
+        None
+            DESCRIPTION.
+
+        '''
         if not (name in self.stats_definitions.keys()):
             self.stats_definitions[name] = function
             self.stats_values[name] = []
     
     def update_statistics(self, model: Model) -> None:
+        '''
+        
+
+        Parameters
+        ----------
+        model : Model
+            DESCRIPTION.
+
+        Returns
+        -------
+        None
+            DESCRIPTION.
+
+        '''
         for statistic in self.stats_definitions.keys():
             self.stats_values[statistic].append(self.stats_definitions[statistic](model))
             
     def save_statistics(self, file_name: str = "statistics") -> None:
+        '''
+        
+
+        Parameters
+        ----------
+        file_name : str, optional
+            DESCRIPTION. The default is "statistics".
+
+        Returns
+        -------
+        None
+            DESCRIPTION.
+
+        '''
         pickle.dump(self.stats_values, open(file_name+".pickle", "rb"))
 
 class MeanEntropy(Statistic):
@@ -78,32 +155,32 @@ class MeanProximity(Statistic):
         '''
         return model.J
 
-def update_statistics(M: Model, statistics: Dict):
-    """Updates statistics extracted from the model.
 
-    Args:
-        M (Model): A model instance.
-        statistics (Dict): A dictionary with the statistics arrays to be updated.
-    """ 
-    statistics['H'].append(M.H)
-    dist = M.compute_info_distribution()
-    for code in statistics['Distribution'].keys():
-        statistics['Distribution'][code].append(dist[code])
-    # statistics['pi - seed = {}'.format(M.seed)].append(M.pi)
-    
-def compute_info_distribution(M: Model):
-    hist = {info:0 for info in M.indInfo(0).P.keys()}
-    N = 0
-    
-    for node in M.G:
-        for info in M.indInfo(node).L[0]:
-            info = binary_to_string(info)
-            hist[info] += 1
-            N += 1
-    
-    dist = {}
-    
-    for info in hist.keys():
-        dist[info] = hist[info]/N
-    
-    return dist
+class InformationDistribution(Statistic):
+    def __call__(self, model: Model) -> np.array:
+        '''
+        
+
+        Parameters
+        ----------
+        model : Model
+            DESCRIPTION.
+
+        Returns
+        -------
+        probability_distribution : TYPE
+            DESCRIPTION.
+
+        '''
+        histogram = np.arange(2**code_length)
+        num_codes = N * memory_size
+        
+        for node in model.G:
+            memory = (powers_of_two*node.L[0]).sum(axis = 1)
+            for code in memory:
+                histogram[code] += 1
+                
+        probability_distribution = histogram / num_codes
+        
+        return probability_distribution
+        
