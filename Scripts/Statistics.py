8485fa7def7e8e96a3bbb68f35af258905a88688
RedRoboto <patrick.oliveira@aluno.ufabc.edu.br>
2021-09-01 23:41:13 -0300

    Progamando estatisticas

--- Scripts/Statistics.py
+++ Scripts/Statistics.py
@@ -1,6 +1,82 @@
 from Scripts.Model import Model
-from typing import Dict
+from typing import Dict, Callable
 from Scripts.Memory import binary_to_string
+import pickle
+
+class Statistic:
+    def __call__(self, model: Model) -> float:
+        raise NotImplementedError
+        
+class StatisticHandler:
+    def __init__(self):
+        self.stats_definitions = {}
+        self.stats_values      = {}
+
+    def new_statistic(self, name: str, function: Statistic) -> None:
+        if not (name in self.stats_definitions.keys()):
+            self.stats_definitions[name] = function
+            self.stats_values[name] = []
+    
+    def update_statistics(self, model: Model) -> None:
+        for statistic in self.stats_definitions.keys():
+            self.stats_values[statistic].append(self.stats_definitions[statistic](model))
+            
+    def save_statistics(self, file_name: str = "statistics") -> None:
+        pickle.dump(self.stats_values, open(file_name+".pickle", "rb"))
+
+class MeanEntropy(Statistic):
+    def __call__(self, model: Model) -> float:
+        '''
+        
+
+        Parameters
+        ----------
+        model : Model
+            DESCRIPTION.
+
+        Returns
+        -------
+        float
+            DESCRIPTION.
+
+        '''
+        return model.H
+    
+class MeanPolarity(Statistic):
+    def __call__(self, model: Model) -> float:
+        '''
+            
+
+        Parameters
+        ----------
+        model : Model
+            DESCRIPTION.
+
+        Returns
+        -------
+        float
+            DESCRIPTION.
+
+        '''
+        return model.pi
+    
+class MeanProximity(Statistic):
+    def __call__(self, model: Model) -> float:
+        '''
+        
+
+        Parameters
+        ----------
+        model : Model
+            DESCRIPTION.
+
+        Returns
+        -------
+        float
+            DESCRIPTION.
+
+        '''
+        return model.J
 
 def update_statistics(M: Model, statistics: Dict):
     """Updates statistics extracted from the model.
