0d06f86fde7f473f81b88f57a7a20c82d88695f6
Patrick Oliveira <patrick.oliveira@aluno.ufabc.edu.br>
2024-01-03 23:42:06 -0300

    Debugging

--- opdynamics/simulation/experimentation.py
+++ opdynamics/simulation/experimentation.py
@@ -15,19 +15,19 @@ from opdynamics.utils.types import Parameters
 def worker(worker_input: Tuple[int, List[Parameters]]):
     worker_id  = worker_input[0]
     param_list = worker_input[1]
-    
+
     if type(param_list) != list:
         param_list = [param_list]
-        
+
     num_params = len(param_list)
     print(f"[WORKER {worker_id}] Starting processes. Number of parameters combinations to simulate: {num_params}")
-        
+
     for k, params in enumerate(param_list):
         print(f"[WORKER {worker_id}] Param set {k + 1} out of {num_params}")
-        
+
         simulation_params_tuple = tuple(params.values())[:-5]
         output_path = Path(params["results_path"]) / param_to_hash(simulation_params_tuple)
-        
+
         new_experiment = not output_path.exists()
         if new_experiment:
             print(f"[WORKER {worker_id}] No previous runs found. Creating new model.")
@@ -35,43 +35,43 @@ def worker(worker_input: Tuple[int, List[Parameters]]):
             last_run = -1
         else:
             model, last_run = load_experiment(output_path)
-            
+
             if last_run == -2 or last_run == int(params['num_repetitions']):
                 print(f"[WORKER {worker_id}] This parameter combination has already been simulated up to {params['num_repetitions']} or up to an acceptable error threshold.")
                 continue
             else:
                 print(f"[WORKER {worker_id}] Loaded existing model. Last run: {last_run}.")
-            
+
         print(f"[WORKER {worker_id}] Starting simulation.")
         start = time()
-        
+
         elapsed_time, statistic_handler = evaluate_model(
             model,
             T = params["T"],
             num_repetitions = params["num_repetitions"],
             early_stop = params["early_stop"],
             epsilon = params["epsilon"],
-            last_run = last_run, 
-            verbose = True, 
-            save_runs = True, 
+            last_run = last_run,
+            verbose = True,
+            save_runs = True,
             save_path = output_path,
             worker_id = worker_id,
         )
-        
+
         end = time()
         print(f"[WORKER {worker_id}] Finished simulation. Elapsed time (s): {end - start}")
-    
+
 def make_new_experiment(params: Parameters, output_path: Path, **kwargs) -> Model:
     os.makedirs(output_path)
     model = initialize_model(**params, **kwargs)
 
     with open(output_path / "initial_model.pkl", "wb") as file:
         pickle.dump(model, file)
-    
+
     f = open(output_path / "last_run.txt", "w")
     f.write("-1")
-    f.close() 
-    
+    f.close()
+
     return model
 
 def load_experiment(output_path: Path) -> Tuple[Model, int]:
@@ -79,18 +79,18 @@ def load_experiment(output_path: Path) -> Tuple[Model, int]:
     last_run = int(f.read())
     f.close()
     model = pickle.load(open(output_path / "initial_model.pkl", "rb"))
-    
+
     return model, last_run
 
 def validate_params(params: dict) -> bool:
     if params["alpha"] + params["omega"] > 1:
         return False
-    
+
     return True
 
 def build_param_list(input_params: dict) -> List[Parameters]:
     general_params = input_params["general_params"]
-    
+
     simulation_params_keys = input_params['simulation_params'].keys()
     simulation_params = input_params['simulation_params']
     simulation_params = list(product(*simulation_params.values()))
@@ -101,5 +101,4 @@ def build_param_list(input_params: dict) -> List[Parameters]:
     simulation_params = [x | general_params for x in simulation_params if validate_params(x)]
 
     return simulation_params
-    
-        
+
--- opdynamics/simulation/simulation.py
+++ opdynamics/simulation/simulation.py
@@ -13,19 +13,19 @@ from opdynamics.model.statistics import StatisticHandler, error_curve
 
 
 def initialize_model(
-    graph_type: str, 
+    graph_type: str,
     network_size: int,
-    memory_size: int, 
+    memory_size: int,
     code_length: int,
-    kappa: float, 
+    kappa: float,
     lambd: float,
-    alpha: float, 
+    alpha: float,
     omega: float,
     gamma: float,
     preferential_attachment: float = None,
-    polarization_grouping_type: int = 0, 
-    degree: int = None, 
-    edge_prob: float = None, 
+    polarization_grouping_type: int = 0,
+    degree: int = None,
+    edge_prob: float = None,
     verbose: bool = False,
     worker_id: int = None,
     distribution: str = "binomial",
@@ -35,27 +35,27 @@ def initialize_model(
     worker_id = f"[WORKER {worker_id}] " if worker_id is not None else ""
     if verbose:
         print(worker_id + "Initializing model.")
-    
+
     start = time.time()
     initial_model = Model(
-        graph_type = graph_type, 
-        network_size = network_size, 
-        memory_size = memory_size, 
-        code_length = code_length, 
-        kappa = kappa, 
-        lambd = lambd, 
-        alpha = alpha, 
-        omega = omega, 
-        gamma = gamma, 
+        graph_type = graph_type,
+        network_size = network_size,
+        memory_size = memory_size,
+        code_length = code_length,
+        kappa = kappa,
+        lambd = lambd,
+        alpha = alpha,
+        omega = omega,
+        gamma = gamma,
         preferential_attachment = preferential_attachment,
-        polarization_grouping_type = polarization_grouping_type, 
-        d = degree, 
+        polarization_grouping_type = polarization_grouping_type,
+        d = degree,
         p = edge_prob,
         distribution = distribution,
         **kwargs
     )
     model_initialization_time = time.time() - start
-    
+
     if verbose:
         print(
             worker_id +\
@@ -63,12 +63,12 @@ def initialize_model(
                 np.round(model_initialization_time/60, 2)
             )
         )
-    
+
     return initial_model
 
 def init_statistic_handler(s_names: List[str] = None) -> StatisticHandler:
     statistic_handler = StatisticHandler()
-    
+
     if s_names is None:
         s_names = [
             "Entropy",
@@ -78,32 +78,32 @@ def init_statistic_handler(s_names: List[str] = None) -> StatisticHandler:
             "Acceptance",
             "Transmission"
         ]
-    
+
     for name in s_names:
         try:
             statistic_handler.new_statistic(name, stats_dict[name]())
         except Exception:
             print(f"Error building statistic: {name}")
-        
+
     return statistic_handler
 
 def Parallel_evaluateModel(
     initial_model: Model,
-    T: int, 
-    num_repetitions: int, 
+    T: int,
+    num_repetitions: int,
     verbose: bool = False
 ) -> Tuple[float, List[Dict], Dict]:
     pass
 
 def evaluate_model(
     initial_model: Model,
-    T: int, 
+    T: int,
     num_repetitions: int = 1,
     early_stop: bool = False,
     epsilon: float = 1e-5,
-    last_run: int = -1, 
+    last_run: int = -1,
     verbose: bool = False,
-    save_runs: bool = False, 
+    save_runs: bool = False,
     save_path: Union[Path, str] = None,
     worker_id: int = None,
     *args,
@@ -116,37 +116,37 @@ def evaluate_model(
     if verbose:
         print(worker_id + "Model evaluation started.")
         print(worker_id + f"Number of repetitions = {num_repetitions}")
-        
+
     simulation_time = []
     statistic_handler = init_statistic_handler()
-    
+
     current_run = last_run + 1
     for repetition in range(current_run, num_repetitions):
         if verbose:
             print(worker_id + f"Repetition {repetition}/{num_repetitions}")
-        
+
         model = deepcopy(initial_model)
-        
+
         start = time.time()
         for _ in range(T):
             simulate(model)
             statistic_handler.update_statistics(model)
         repetition_time = time.time() - start
         simulation_time.append(repetition_time)
-        
+
         if verbose:
             print(
                 worker_id +\
                 f"Finished repetition {repetition + 1}/{num_repetitions}. "\
                 f"Elapsed time: {np.round(simulation_time[-1]/60, 2)} minutes"
             )
-        
+
         statistic_handler.end_repetition()
-        
+
         if save_runs:
             with open(save_path / f"run_{repetition}_stats.pkl", "wb") as file:
                 pickle.dump(statistic_handler.repetitions[-1], file)
-                
+
         if early_stop:
             errors = error_curve(save_path, T)
             errors = {
@@ -154,10 +154,10 @@ def evaluate_model(
                 "proximity": errors["proximity"][-1],
                 "polarity": errors["polarity"][-1]
             }
-            
+
             print(worker_id + "Last errors:")
             pprint(errors)
-            
+
             if errors["entropy"] <= epsilon and \
                 errors["proximity"] <= epsilon and \
                     errors["polarity"] <= epsilon:
@@ -168,21 +168,21 @@ def evaluate_model(
                         )
                         run_count(-2, save_path)
                         break
-                    
+
         run_count(repetition, save_path)
-       
+
     elapsedTime = sum(simulation_time)
 
     return elapsedTime, statistic_handler
-       
+
 def simulate(M: Model):
     """
-    Execute one iteration of the information propagation model, updating the model's 
-    parameters at the end. 
+    Execute one iteration of the information propagation model, updating the model's
+    parameters at the end.
     Return the execution time (minutes).
 
     Args:
-        M (Model): A model instance.  
+        M (Model): A model instance.
 
     Returns:
         None.
@@ -192,7 +192,7 @@ def simulate(M: Model):
         v_ind = M.indInfo(v)
         received = u_ind.receive_information(
             evaluate_information(
-                distort(v_ind.X, v_ind.DistortionProbability), 
+                distort(v_ind.X, v_ind.DistortionProbability),
                 M.get_acceptance_probability(u, v)
             )
         )
@@ -201,16 +201,16 @@ def simulate(M: Model):
             u_ind.received()
         received = v_ind.receive_information(
             evaluate_information(
-                distort(u_ind.X, u_ind.DistortionProbability), 
+                distort(u_ind.X, u_ind.DistortionProbability),
                 M.get_acceptance_probability(v, u)
             )
         )
         if received:
             u_ind.transmitted()
             v_ind.received()
-    
+
     M.update_model()
-    
+
 def run_count(run: int, path: Path):
     f = open(path / "last_run.txt", "w")
     f.write(str(run))
